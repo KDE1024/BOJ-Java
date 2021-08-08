ab58410c38f1470782558997ff8651f39d9fd004
KIM DAEUN <80617948+KDE1024@users.noreply.github.com>
2021-08-08 17:33:32 +0900

    Add files via upload

---
+++ if/bj01.java
@@ -0,0 +1,31 @@
+package task01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj01 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		int A;
+		int B;
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		A = Integer.parseInt(st.nextToken()) ;
+		B = Integer.parseInt(st.nextToken()) ;
+		if(A>B) {
+			System.out.println(">");
+		}
+		if(A<B) {
+			System.out.println("<");
+		}
+		if(A==B) {
+			System.out.println("==");
+		}
+	
+	}
+
+}
