cdbea494a14d8a3bb90347cd6cc3ca250d0f86a5
KIM DAEUN <80617948+KDE1024@users.noreply.github.com>
2021-08-09 13:39:19 +0900

    A * B

---
+++ In and Out/InOut_10998.java
@@ -0,0 +1,23 @@
+package boj_java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class InOut_10998 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("두 수를 입력하세요.");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a*b);
+		
+	}//main
+
+}//class
