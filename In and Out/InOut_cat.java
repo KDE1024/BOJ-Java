79905b731b7443ad634c2b091185ec25bae5089b
KIM DAEUN <80617948+KDE1024@users.noreply.github.com>
2021-08-17 17:38:29 +0900

    dog, cat
    아주 귀찮았던 강쥐와 고양이를 끝으로 입출력 끝~!

---
+++ In and Out/InOut_cat.java
@@ -0,0 +1,15 @@
+package in_and_out;
+
+public class InOut_cat {
+
+	public static void main(String[] args) {
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		
+	}//main
+
+}//class
---
+++ In and Out/InOut_dog.java
@@ -0,0 +1,14 @@
+package in_and_out;
+
+public class InOut_dog {
+
+	public static void main(String[] args) {
+
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}
